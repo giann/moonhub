7ef73497f15f6f61d0bd49402b08e0d22d58098e
Benoit Giannangeli <giann008@gmail.com>
2017-08-07 08:28:48 +0200

    Project selector logic

--- src/moonhub.lua
+++ src/moonhub.lua
@@ -11,18 +11,65 @@ local View       = moonview.View
 local Model      = moonview.Model
 local Collection = moonview.Collection
 
-local githubEndpoint = "https://api.github.com"
+local ghEndpoint = "https://api.github.com"
+local ghToken    = "?access_token=639ccc89c5d5b919733b25e90f3cee87a80c874d"
 
 coroutine.wrap(function()
-    local m = Model {
-        commits = Collection(fetchj(githubEndpoint .. "/repos/giann/moonview/commits"))
+
+    local projectSelectorView, commitsView
+
+    projectSelectorView = View {
+        target   = "#project-selector",
+        template = "#project-selector-template",
+        model    = Model {
+            user   = nil,
+            project = nil
+        },
+        events   = {
+            ["click #go"] = function(self)
+                self:update()
+            end,
+            ["keydown input"] = function(self, _, event)
+                if (event.key == "Enter") then
+                    self:update()
+                end
+            end
+        }
     }
 
-    local v = View {
-        target   = "#app",
+    projectSelectorView.update = function(self)
+        local nuser = q("input[name=user]").value
+        local nproject = q("input[name=project]").value
+
+        if (nuser ~= self.model.user or nproject ~= self.model.project) then
+            self.model.user = nuser
+            self.model.project = nproject
+
+            coroutine.wrap(function ()
+                commitsView.model.commits = Collection(
+                    fetchj(
+                        ghEndpoint
+                        .. "/repos/"
+                        .. projectSelectorView.model.user
+                        .. "/"
+                        .. projectSelectorView.model.project
+                        .. "/commits"
+                        .. ghToken
+                    )
+                )
+            end)()
+        end
+    end
+
+    commitsView = View {
+        target   = "#commits",
         template = "#commits-template",
-        model    = m,
+        model    = Model {
+            commits = Collection {}
+        }
     }
 
-    v:render()
+    projectSelectorView:render()
+    commitsView:render()
+
 end)()
